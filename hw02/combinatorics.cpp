fa733447bc6fbd034c1c901eaf7ebc251e05e50b
Gabriele Princiotta <gabri@LAPTOP-PB9PB19G.localdomain>
2022-11-05 15:09:30 +0100

    test passed

--- hw02/combinatorics.cpp
+++ hw02/combinatorics.cpp
@@ -2,18 +2,22 @@
 
 
 uint64_t factorial(uint64_t val) {
-    // TODO
-    return 0;
+    if (val == 0)
+        return 1; 
+    else
+        return val * factorial(val-1);
 }
 
 
 uint64_t permutation(uint64_t val, uint64_t val2) {
-    // TODO
+    if (val >= val2)
+        return factorial(val) / factorial(val - val2) ;
     return 0;
 }
 
 
 uint64_t combination(uint64_t val, uint64_t val2) {
-    // TODO
+    if (val >= val2)
+        return factorial(val) / (factorial(val2) * factorial(val - val2));  
     return 0;
 }
--- hw02/run.cpp
+++ hw02/run.cpp
@@ -3,7 +3,7 @@
 
 #include <iostream>
 
-
+    
 int main() {
     // here you can test your functions
     std::cout << "perm(10, 3)=" << permutation(10, 3) << std::endl;
