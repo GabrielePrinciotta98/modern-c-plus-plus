58f2dfdf828a0303442dfb09d341352536348817
Gabriele Princiotta <gabri@LAPTOP-PB9PB19G.localdomain>
2022-12-13 14:25:02 +0100

    hw06: test passed

--- hw05/validator.cpp
+++ hw05/validator.cpp
@@ -6,7 +6,6 @@
 #include "token.h"
 
 namespace sql {
-
 bool is_valid_sql_query(std::vector<Token> tokens) {
   SqlValidator sqlValidator{};
   auto i = tokens.begin();
@@ -27,147 +26,59 @@ bool SqlValidator::is_invalid() const {
   return std::holds_alternative<state::Invalid>(state_);
 }
 
-
-void SqlValidator::handle(Token token) {
-  if (std::holds_alternative<state::Start>(state_)){
-    state_ = transition(std::get<state::Start>(state_), token);
-    return;
-  }
-
-  if (std::holds_alternative<state::Valid>(state_)){
-    state_ = transition(std::get<state::Valid>(state_), token);
-    return;
-  }
-
-  if (std::holds_alternative<state::Invalid>(state_)){
-    state_ = transition(std::get<state::Invalid>(state_), token);
-    return;
-  }
-
-  if (std::holds_alternative<state::SelectStmt>(state_)){
-    state_ = transition(std::get<state::SelectStmt>(state_), token);
-    return;
-  }
-    
-  if (std::holds_alternative<state::AllColumns>(state_)){
-    state_ = transition(std::get<state::AllColumns>(state_), token);
-    return;
-  }
-  
-  if (std::holds_alternative<state::NamedColumn>(state_)){
-    state_ = transition(std::get<state::NamedColumn>(state_), token);
-    return;
-  }
-
-  if (std::holds_alternative<state::MoreColumns>(state_)){
-    state_ = transition(std::get<state::MoreColumns>(state_), token);
-    return;
-  }
-
-  if (std::holds_alternative<state::FromClause>(state_)){
-    state_ = transition(std::get<state::FromClause>(state_), token);
-    return;
-  }
-
-  if (std::holds_alternative<state::TableName>(state_)){
-    state_ = transition(std::get<state::TableName>(state_), token);
-    return;
-  }
-
+void SqlValidator::handle(Token token){
+  state_ = std::visit([&](auto cur) -> State { return transition(cur, token);}, state_);
 }
 
-
 struct TransitionFromStartVisitor {
-  /// Only the `Select` token advances the FSM from the `Start` state
-  /// TODO: Correct the behaviour (DONE?)
   State operator()(token::Select) const { return state::SelectStmt{}; }
-
   /// All the other tokens, put it in the invalid state
-  State operator()(token::From) const { return state::Invalid{}; }
-  State operator()(token::Comma) const { return state::Invalid{}; }
-  State operator()(token::Asterisks) const { return state::Invalid{}; }
-  State operator()(token::Semicolon) const { return state::Invalid{}; }
-  State operator()(token::Identifier) const { return state::Invalid{}; }
+  State operator()(auto) const { return state::Invalid{}; }
 };
 
 struct TransitionFromValidVisitor {
-  /// Only the `Select` token advances the FSM from the `Start` state
-  /// TODO: Correct the behaviour (DONE?)
   State operator()(token::Semicolon) const { return state::Valid{}; }
-
   /// All the other tokens, put it in the invalid state
-  State operator()(token::Select) const { return state::Invalid{}; }
-  State operator()(token::From) const { return state::Invalid{}; }
-  State operator()(token::Comma) const { return state::Invalid{}; }
-  State operator()(token::Asterisks) const { return state::Invalid{}; }
-  State operator()(token::Identifier) const { return state::Invalid{}; }
+  State operator()(auto) const { return state::Invalid{}; }
 };
 
 
 struct TransitionFromSelectStmtVisitor {
   State operator()(token::Asterisks) const { return state::AllColumns{}; }
   State operator()(token::Identifier) const { return state::NamedColumn{}; }
-
   /// All the other tokens, put it in the invalid state
-  State operator()(token::Select) const { return state::Invalid{}; }
-  State operator()(token::From) const { return state::Invalid{}; }
-  State operator()(token::Comma) const { return state::Invalid{}; }
-  State operator()(token::Semicolon) const { return state::Invalid{}; }
+  State operator()(auto) const { return state::Invalid{}; }
 };
 
 struct TransitionFromAllColumnsVisitor {
   State operator()(token::From) const { return state::FromClause{}; }
-
   /// All the other tokens, put it in the invalid state
-  State operator()(token::Asterisks) const { return state::Invalid{}; }
-  State operator()(token::Identifier) const { return state::Invalid{}; }
-  State operator()(token::Select) const { return state::Invalid{}; }
-  State operator()(token::Comma) const { return state::Invalid{}; }
-  State operator()(token::Semicolon) const { return state::Invalid{}; }
+  State operator()(auto) const { return state::Invalid{}; }
 };
 
 struct TransitionFromNamedColumnVisitor {
   State operator()(token::From) const { return state::FromClause{}; }
   State operator()(token::Comma) const { return state::MoreColumns{}; }
-
   /// All the other tokens, put it in the invalid state
-  State operator()(token::Asterisks) const { return state::Invalid{}; }
-  State operator()(token::Identifier) const { return state::Invalid{}; }
-  State operator()(token::Select) const { return state::Invalid{}; }
-  State operator()(token::Semicolon) const { return state::Invalid{}; }
+  State operator()(auto) const { return state::Invalid{}; }
 };
 
 struct TransitionFromMoreColumnsVisitor {
   State operator()(token::Identifier) const { return state::NamedColumn{}; }
-
   /// All the other tokens, put it in the invalid state
-  State operator()(token::Comma) const { return state::Invalid{}; }
-  State operator()(token::From) const { return state::Invalid{}; }
-  State operator()(token::Asterisks) const { return state::Invalid{}; }
-  State operator()(token::Select) const { return state::Invalid{}; }
-  State operator()(token::Semicolon) const { return state::Invalid{}; }
+  State operator()(auto) const { return state::Invalid{}; }
 };
 
 struct TransitionFromFromClauseVisitor {
   State operator()(token::Identifier) const { return state::TableName{}; }
-
   /// All the other tokens, put it in the invalid state
-  State operator()(token::Comma) const { return state::Invalid{}; }
-  State operator()(token::From) const { return state::Invalid{}; }
-  State operator()(token::Asterisks) const { return state::Invalid{}; }
-  State operator()(token::Select) const { return state::Invalid{}; }
-  State operator()(token::Semicolon) const { return state::Invalid{}; }
+  State operator()(auto) const { return state::Invalid{}; }
 };
 
 struct TransitionFromTableNameVisitor {
   State operator()(token::Semicolon) const { return state::Valid{}; }
-
   /// All the other tokens, put it in the invalid state
-  State operator()(token::Identifier) const { return state::Invalid{}; }
-  State operator()(token::Comma) const { return state::Invalid{}; }
-  State operator()(token::From) const { return state::Invalid{}; }
-  State operator()(token::Asterisks) const { return state::Invalid{}; }
-  State operator()(token::Select) const { return state::Invalid{}; }
+  State operator()(auto) const { return state::Invalid{}; }
 };
 
 State transition(state::Start, Token token) {
@@ -175,42 +86,34 @@ State transition(state::Start, Token token) {
 }
 
 State transition(state::Valid, Token token) {
-  // TODO: Implement //maybe not since they are ending states (?)
   return std::visit(TransitionFromValidVisitor{}, token.value());
 }
 
-State transition(state::Invalid, Token token) {
-  // TODO: Implement //maybe not since they are ending states (?)
+State transition(state::Invalid, Token) {
   return state::Invalid{};
 }
 
 State transition(state::SelectStmt, Token token) {
-  // TODO: Implement
   return std::visit(TransitionFromSelectStmtVisitor{}, token.value());
 }
 
 State transition(state::AllColumns, Token token) {
-  // TODO: Implement
   return std::visit(TransitionFromAllColumnsVisitor{}, token.value());
 }
 
 State transition(state::NamedColumn, Token token) {
-  // TODO: Implement
   return std::visit(TransitionFromNamedColumnVisitor{}, token.value());
 }
 
 State transition(state::MoreColumns, Token token) {
-  // TODO: Implement
   return std::visit(TransitionFromMoreColumnsVisitor{}, token.value());
 }
 
 State transition(state::FromClause, Token token) {
-  // TODO: Implement
   return std::visit(TransitionFromFromClauseVisitor{}, token.value());
 }
 
 State transition(state::TableName, Token token) {
-  // TODO: Implement
   return std::visit(TransitionFromTableNameVisitor{}, token.value());
 }
 
--- hw06/vector.cpp
+++ hw06/vector.cpp
@@ -1,16 +1,328 @@
 #include "vector.h"
 
 #include <iterator>
+#include <stdexcept>
+#include <numeric>
+#include <cmath>
 
 
 namespace linalg {
+
+Vector::Vector(std::size_t n)
+    : data_{std::vector<float>(n)}{}
+
+Vector::Vector(std::size_t n, float val)
+    : data_{std::vector<float>(n, val)}{}
+
+Vector::Vector(std::initializer_list<float> list) 
+    : data_{list} {}
+
+
+void Vector::assign(float val)
+{
+    data_.assign(data_.size(), val);
+}
+
+void Vector::assign(Vector v)
+{
+    data_.assign(v.begin(), v.end());
+}
+
+Vector& Vector::operator=(float val) 
+{
+    assign(val);
+    return *this;
+}
+
+Vector& Vector::operator+=(float val)
+{
+    std::transform(data_.begin(), data_.end(), data_.begin(), 
+        [val](float i){ return i + val; });
+    return *this;
+}
+
+Vector& Vector::operator-=(float val)
+{
+    std::transform(data_.begin(), data_.end(), data_.begin(), 
+        [val](float i){ return i - val; });
+    return *this;
+}
+
+Vector& Vector::operator*=(float val)
+{
+    std::transform(data_.begin(), data_.end(), data_.begin(), 
+        [val](float i){ return i * val; });
+    return *this;
+}
+
+Vector& Vector::operator/=(float val)
+{
+    if (val != 0)
+    {
+        std::transform(data_.begin(), data_.end(), data_.begin(), 
+            [val](float i){ return i / val; });
+    }
+    return *this;
+}
+
+Vector& Vector::operator+=(const Vector& y)
+{
+    if(this -> size() != y.size()) throw std::invalid_argument("");
+    std::transform(data_.begin(), data_.end(), y.data_.begin(), data_.begin(), 
+        [](float i, float j){ return i + j; });
+    return *this;
+}
+
+Vector& Vector::operator-=(const Vector& y)
+{
+    if(this -> size() != y.size()) throw std::invalid_argument("");
+    std::transform(data_.begin(), data_.end(), y.data_.begin(), data_.begin(), 
+        [](float i, float j){ return i - j; });
+    return *this;
+}
+
+std::size_t Vector::size() const
+{
+    return data_.size();
+}
+
+Vector::iterator Vector::begin()
+{
+    return data_.begin();
+}
+
+Vector::iterator Vector::end()
+{
+    return data_.end();
+}
+
+Vector::const_iterator Vector::begin() const
+{
+    return data_.cbegin();
+}
+
+Vector::const_iterator Vector::end() const
+{
+    return data_.cend();
+}
+
+Vector::const_iterator Vector::cbegin() const
+{
+    return data_.cbegin();
+}
+
+Vector::const_iterator Vector::cend() const
+{
+    return data_.cend();
+}
+
+float& Vector::operator[](int idx)
+{
+    size_t size = data_.size();
+    if (idx < 0 && idx*-1 < size)
+        return data_.at(idx + size);
+    return data_.at(idx);
+}
+
+const float& Vector::operator[](int idx) const
+{
+    auto size = data_.size();
+    if (idx < 0 && idx*-1 < size)
+        return data_.at(idx + size);
+    return data_.at(idx);
+}
+
+float& Vector::coeff(int idx)
+{
+    return data_.at(idx);
+}
+
+const float& Vector::coeff(int idx) const
+{
+    return data_.at(idx);
+}
+
 auto operator<<(std::ostream& ostr, const Vector& x) -> std::ostream& {
     ostr << "[ ";
     std::copy(x.begin(), x.end(), std::ostream_iterator<float>(ostr, " "));
     ostr << "]";
     return ostr;
 }
+
+float min(const Vector& x)
+{
+    if (x.size() == 0) throw std::invalid_argument("");
+    return *std::min_element(x.begin(), x.end());
 }
+
+float max(const Vector& x)
+{
+    if (x.size() == 0) throw std::invalid_argument("");
+    return *std::max_element(x.begin(), x.end());
+}
+
+std::size_t argmin(const Vector& x)
+{
+    if (x.size() == 0) throw std::invalid_argument("");
+    return std::distance(x.begin(), std::min_element(x.begin(), x.end()));
+}
+
+std::size_t argmax(const Vector& x)
+{
+    if (x.size() == 0) throw std::invalid_argument("");
+    return std::distance(x.begin(), std::max_element(x.begin(), x.end()));
+}
+
+std::size_t non_zeros(const Vector& x)
+{
+    int n_zeros = std::count(x.begin(), x.end(), 0);
+    return x.size() - n_zeros;
+}
+
+float sum(const Vector& x)
+{
+    return std::accumulate(x.begin(), x.end(), 0.0f);
+}
+
+float prod(const Vector& x)
+{
+    return std::accumulate(x.begin(), x.end(), 1.0f, std::multiplies<float>());
+}
+
+float dot(const Vector &x, const Vector &y)
+{
+    if(x.size() != y.size()) throw std::invalid_argument("");
+    
+    std::vector<float> sums;
+    std::transform(x.begin(), x.end(), y.begin(), std::back_inserter(sums), 
+        [](float i, float j){ return i * j; });
+    return std::accumulate(sums.begin(), sums.end(), 0.0f);
+}
+
+float norm(const Vector &x)
+{
+    return sqrtf(dot(x, x));
+}
+
+void normalize(Vector& x)
+{
+    x /= norm(x);
+}
+
+Vector normalized(const Vector& x)
+{
+    Vector result = x;
+    normalize(result);
+    return result;
+}
+
+Vector floor(const Vector& x)
+{
+    std::vector<float> output;
+    std::transform(x.begin(), x.end(), std::back_inserter(output), 
+        [](float i){ return std::floor(i); });
+    Vector result = Vector(output.size());
+    for (int i = 0; i < output.size(); i++)
+    {
+        result[i] = output[i];
+    }
+
+    return result;
+}
+
+Vector ceil(const Vector& x)
+{
+    std::vector<float> output;
+    std::transform(x.begin(), x.end(), std::back_inserter(output), 
+        [](float i){ return std::ceil(i); });
+    Vector result = Vector(output.size());
+    for (int i = 0; i < output.size(); i++)
+    {
+        result[i] = output[i];
+    }
+
+    return result;
+}
+
+Vector operator+(const Vector& x)
+{
+    Vector result = x;
+    return result;
+}
+
+Vector operator-(const Vector& x)
+{
+    Vector result = x;
+    return result *= -1;
+}
+
+Vector operator+(const Vector& x, const Vector& y)
+{
+    Vector result = x;
+    result += y;
+    return result;
+}
+
+Vector operator-(const Vector& x, const Vector& y)
+{
+    Vector result = x;
+    result -= y;
+    return result;
+}
+
+Vector operator+(const Vector& x, float val)
+{
+    Vector result = x;
+    result += val;
+    return result;
+}
+
+Vector operator-(const Vector& x, float val)
+{
+    Vector result = x;
+    result -= val;
+    return result;
+}
+
+Vector operator*(const Vector& x, float val)
+{
+    Vector result = x;
+    result *= val;
+    return result;
+}
+
+Vector operator/(const Vector& x, float val)
+{
+    Vector result = x;
+    result /= val;
+    return result;
+}
+
+Vector operator+(float val, const Vector& x)
+{
+    Vector result = x;
+    result += val;
+    return result;
+}
+
+Vector operator-(float val, const Vector& x)
+{
+    Vector result = x;
+    result = -result + val;
+    return result;
+}
+
+Vector operator*(float val, const Vector& x)
+{
+    Vector result = x;
+    result *= val;
+    return result;
+}
+
+
+}
+
+
 
 /*
  * Please Implement all functions from the header file here!#
